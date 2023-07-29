3e3c5c564feb99518b7472c458c264ad974e126c
Anna Shubkina <anna.shubkina02@gmail.com>
2023-07-29 13:21:19 +0400

    bugs fixed

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ dp = Dispatcher(bot)
 
 vowels = ["а", "о", "у", "ы", "э", "я", "ё", "ю", "и", "е"]
 lat_vowels = ["a", "e", "y", "u", "i", "o"]
+lat_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+               'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+               'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 
 @dp.message_handler(commands=["start"])
@@ -50,49 +54,56 @@ async def reply_to_message(message: types.Message):
 
     for word in words:
         new_word = [x for x in word]
+        try:
+            while not new_word[0] in vowels and not new_word[0] in lat_vowels:
+                del new_word[0]
+        except IndexError:
+            pass
 
-        while not new_word[0] in vowels and not new_word[0] in lat_vowels:
-            del new_word[0]
-
-        for x in range(len(vowels)):
-            if new_word == vowels[x]:
-                token = ""
-            else:
-                token = "".join(new_word[1:])
-
-        for y in range(len(lat_vowels)):
-            if new_word == lat_vowels[y]:
-                token = ""
-            else:
-                token = "".join(new_word[1:])
-
-        if new_word[0] == "а" or new_word[0] == "я":
-            answer = word + "-" + "хуя" + token
-        elif new_word[0] == "о" or new_word[0] == "ё":
-            answer = word + "-" + "хуё" + token
-        elif new_word[0] == "у" or new_word[0] == "ю":
-            answer = word + "-" + "хую" + token
-        elif new_word[0] == "ы" or new_word[0] == "и":
-            answer = word + "-" + "хуи" + token
-        elif new_word[0] == "э" or new_word[0] == "е":
-            answer = word + "-" + "хуе" + token
-        elif new_word[0] == "a":
-            answer = word + "-" + "huya" + token
-        elif new_word[0] == "e":
-            answer = word + "-" + "huye" + token
-        elif new_word[0] == "y":
-            answer = word + "-" + "huye" + token
-        elif new_word[0] == "u":
-            answer = word + "-" + "huyu" + token
-        elif new_word[0] == "i":
-            answer = word + "-" + "huya" + token
-        elif new_word[0] == "o":
-            answer = word + "-" + "hyuo" + token
+        if new_word == [] and word[0] in lat_letters:
+            answers += word + '-huye' + word + " "
+        elif new_word == []:
+            answers += word + '-хуе' + word + " "
         else:
-            answer = word + "-" + "хуе" + token
+            for x in range(len(vowels)):
+                if new_word == vowels[x]:
+                    token = ""
+                else:
+                    token = "".join(new_word[1:])
+
+            for y in range(len(lat_vowels)):
+                if new_word == lat_vowels[y]:
+                    token = ""
+                else:
+                    token = "".join(new_word[1:])
+
+            if new_word[0] == "а" or new_word[0] == "я":
+                answer = word + "-" + "хуя" + token
+            elif new_word[0] == "о" or new_word[0] == "ё":
+                answer = word + "-" + "хуё" + token
+            elif new_word[0] == "у" or new_word[0] == "ю":
+                answer = word + "-" + "хую" + token
+            elif new_word[0] == "ы" or new_word[0] == "и":
+                answer = word + "-" + "хуи" + token
+            elif new_word[0] == "э" or new_word[0] == "е":
+                answer = word + "-" + "хуе" + token
+            elif new_word[0] == "a":
+                answer = word + "-" + "huya" + token
+            elif new_word[0] == "e":
+                answer = word + "-" + "huye" + token
+            elif new_word[0] == "y":
+                answer = word + "-" + "huye" + token
+            elif new_word[0] == "u":
+                answer = word + "-" + "huyu" + token
+            elif new_word[0] == "i":
+                answer = word + "-" + "huya" + token
+            elif new_word[0] == "o":
+                answer = word + "-" + "huyo" + token
+            else:
+                answer = word + "-" + "хуе" + token
 
 
-        answers += (answer + " ")
+            answers += (answer + " ")
 
     await bot.send_message(message.from_user.id, answers)
 
